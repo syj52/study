7590e0e14024f377f0e4fb6dd348bb6a9bbddf01
syj52 <2570340595@qq.com>
2024-09-23 22:04:12 +0800

    first commit

---
+++ firstApp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div> -->
+  <div>
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from "./views/element/ElementView.vue";
+export default {
+  components: { ElementView },
+  data() {
+    return {
+      massage: "hello vue",
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ firstApp/src/views/element/ElementView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-row>
+      <el-row>
+        <el-button disabled>默认按钮</el-button>
+        <el-button type="primary" disabled>主要按钮</el-button>
+        <el-button type="success" disabled>成功按钮</el-button>
+        <el-button type="info" disabled>信息按钮</el-button>
+        <el-button type="warning" disabled>警告按钮</el-button>
+        <el-button type="danger" disabled>危险按钮</el-button>
+      </el-row>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
